011d6939d8a7421a3e5962885223a0ac6ad8b8a5
ARYANCHAURASIA <aryanchaurasi20@gmail.com>
2025-01-13 01:59:10 +0530

    Added program to calculate the Average of Sam Mark

---
+++ Day-1/Level-1/SamAverage.java
@@ -0,0 +1,11 @@
+public class SamAverage
+{
+	public static void main(String args[])
+	{
+		//initialising all the marks and storing their information in their respective name	
+		double mathMark=94,physicsMark=95,chemistryMark=96;
+		double averagePercentage=(mathMark+physicsMark+chemistryMark)/3;//Calculating the avergae percentage 
+		System.out.println("Sam’s average mark in PCM is "+ averagePercentage);
+		
+	}
+}
