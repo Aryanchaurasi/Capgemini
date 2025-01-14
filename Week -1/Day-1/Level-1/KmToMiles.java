58562d39365b803937c9d34542ac66d8db1e897d
ARYANCHAURASIA <aryanchaurasi20@gmail.com>
2025-01-14 13:10:38 +0530

    Added a new program

---
+++ Week -1/Day-1/Level-1/KmToMiles.java
@@ -0,0 +1,10 @@
+public class KmToMiles
+{
+	public static void main(String [] args)
+	{
+		double km=10.8;//initializing the value of km as 10.8
+		double miles =km*1.6; // calculating the value of km in miles 
+		System.out.println("The distance "+km+" km in miles is "+ miles);
+		
+	}
+}
